4309695c00f20c51c501ce9915d69682cc05b897
Loïc SENCE <lsence@snapp.fr>
2018-06-26 15:28:32 +0200

    Remove client.continue_timeout

--- lib/mixpanel-ruby/consumer.rb
+++ lib/mixpanel-ruby/consumer.rb
@@ -126,7 +126,6 @@ module Mixpanel
       client = Net::HTTP.new(uri.host, uri.port)
       client.use_ssl = true
       client.open_timeout = 10
-      client.continue_timeout = 10
       client.read_timeout = 10
       client.ssl_timeout = 10
 
